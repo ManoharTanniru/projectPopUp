ee9cf01ac8e480cb84701e51c9b6f5bad4a1db07
ManoharTanniru <94604506+ManoharTanniru@users.noreply.github.com>
2022-01-25 12:15:45 +0530

    Update lwcCustomModal.js

--- lwcCustomModal.js
+++ lwcCustomModal.js
@@ -155,22 +155,7 @@ export default class LwcCustomModal extends NavigationMixin(LightningElement) {
 
 
 
-    //  @api recordId;
-    // fileData
-    // openfileUpload(event) {
-    //     const file = event.target.files[0]
-    //     var reader = new FileReader()
-    //     reader.onload = () => {
-    //         var base64 = reader.result.split(',')[1]
-    //         this.fileData = {
-    //             'filename': file.name,
-    //             'base64': base64,
-    //             'recordId': this.recordId
-    //         }
-    //         console.log(this.fileData)
-    //     }
-    //     reader.readAsDataURL(file)
-    // }
+   
 
     // toast(title) {
     //     const toastEvent = new showToastEvent({
